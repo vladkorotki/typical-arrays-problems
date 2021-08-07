fd2e7b5356731e2974d993efb9680df78e63fa04
Vlad Korotki <vladkorotki92@gmail.com>
2021-08-08 00:07:14 +0300

    feat: task typical-arrays-problems is ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) return 0;
+  if (array.length > 1 && array != undefined) {
+    array = array.sort((a, b) => a - b);
+    return array[0];
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length == 0) return 0;
+  if (array.length > 1 && array != undefined) {
+    array = array.sort((a, b) => a - b);
+    return array[array.length - 1];
+  }
+
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) return 0;
+  if (array.length > 1 && array != undefined) {
+    let result = array.reduce((prev, item) => prev + item) / array.length;
+    return result;
+  }
+
 }
